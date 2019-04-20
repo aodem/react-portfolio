c9c09ece33d258250a9e2169893203c385f6c40b
aodem <43295961+aodem@users.noreply.github.com>
2019-04-20 16:11:36 -0700

    Updates

---
+++ precache-manifest.15c04a1b8606fe7e07161a0b2348fcd0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "042b196c7553594ac722",
+    "url": "/aodem/react-portfolio.git/static/js/runtime~main.23abcfdb.js"
+  },
+  {
+    "revision": "5c07038e202a87320d95",
+    "url": "/aodem/react-portfolio.git/static/js/main.2c6b925a.chunk.js"
+  },
+  {
+    "revision": "e552493d30e64551dfd8",
+    "url": "/aodem/react-portfolio.git/static/js/2.e28cec7d.chunk.js"
+  },
+  {
+    "revision": "5c07038e202a87320d95",
+    "url": "/aodem/react-portfolio.git/static/css/main.13f3eafb.chunk.css"
+  },
+  {
+    "revision": "71e053462c07d4a4cb88343c69c8b9d4",
+    "url": "/aodem/react-portfolio.git/index.html"
+  }
+];
